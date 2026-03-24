09c01df9bc77703ee12b4f21341743d62d761f05
Abright17 <samuelamoussango@gmail.com>
2026-03-24 09:32:00 -0400

    démarrage des services #1

--- installer-wordpress.bash
+++ installer-wordpress.bash
@@ -5,3 +5,9 @@ sudo apt update -y
 echo "=== Installation Apache, MySQL, PHP ==="
 sudo apt install -y apache2 mysql-server php libapache2-mod-php php-mysql wget tar curl
 
+
+echo "=== Démarrage des services ==="
+sudo systemctl enable apache2
+sudo systemctl start apache2
+sudo systemctl enable mysql
+sudo systemctl start mysql
