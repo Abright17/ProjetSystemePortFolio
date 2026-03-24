d243e44712173fef46bc1310ea197b9d30cedeaf
Abright17 <samuelamoussango@gmail.com>
2026-03-24 09:26:00 -0400

    Installation des dépendances #1

--- installer-wordpress.bash
+++ installer-wordpress.bash
@@ -2,3 +2,6 @@
 set -e
 echo "=== Mise à jour des paquets ==="
 sudo apt update -y
+echo "=== Installation Apache, MySQL, PHP ==="
+sudo apt install -y apache2 mysql-server php libapache2-mod-php php-mysql wget tar curl
+
